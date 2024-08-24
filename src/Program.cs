2ee7a3adece81233c1f123e453e236a0d910958d
Martin A. Ortega <lynxestudio@outlook.com>
2024-08-24 00:54:46 -0600

    Adds gitignore to the project

---
+++ src/Program.cs
@@ -0,0 +1,20 @@
+﻿/*
+ * Created by SharpDevelop.
+ * User: Administrator
+ * Date: 4/29/2017
+ * Time: 3:10 AM
+ * 
+ * To change this template use Tools | Options | Coding | Edit Standard Headers.
+ */
+using System;
+
+namespace Samples.CSharp.NpgsqlErrors
+{
+	class Program
+	{
+		public static void Main(string[] args)
+		{
+			
+		}
+	}
+}
